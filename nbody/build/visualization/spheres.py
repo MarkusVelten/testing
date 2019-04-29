12ca62ebeae29d9575561801586d55513229d164
Markus Velten <markus.velten@tu-dresden.de>
2019-04-29 17:08:48 +0200

    code cleanup

--- nbody/build/visualization/spheres.py
+++ nbody/build/visualization/spheres.py
@@ -11,7 +11,7 @@ from joblib import Parallel, delayed
 def create_sphere(PosX, PosY, PosZ, force_p, size, mode="theta", size_factor=0.00001):
     s = vtk.vtkSphereSource()
     s.SetRadius(size * size_factor)
-    # s.SetRadius(1)
+
     s.SetCenter(PosX, PosY, PosZ)
     if mode == "theta":
         s.SetStartTheta(0)
@@ -23,7 +23,7 @@ def create_sphere(PosX, PosY, PosZ, force_p, size, mode="theta", size_factor=0.0
 
     ss = vtk.vtkSphereSource()
     ss.SetRadius(size * size_factor)
-    # ss.SetRadius(1)
+
     ss.SetCenter(PosX, PosY, PosZ)
     if mode == "theta":
         ss.SetStartTheta(force_p * 360.0)
@@ -38,8 +38,6 @@ def create_sphere(PosX, PosY, PosZ, force_p, size, mode="theta", size_factor=0.0
 # centers == positions
 # force_data == forces
 def generate_spheres(HForce, CForce, LForce, PosX, PosY, PosZ, force):
-    #data_theta1 = vtk.vtkAppendPolyData()
-    #data_theta2 = vtk.vtkAppendPolyData()
     data_phi1 = vtk.vtkAppendPolyData()
     data_phi2 = vtk.vtkAppendPolyData()
 
@@ -51,21 +49,15 @@ def generate_spheres(HForce, CForce, LForce, PosX, PosY, PosZ, force):
         else:
             RelForce = force[i] / ForceSum
 
-        #t, tt = create_sphere(PosX[i], PosY[i], PosZ[i], abs(RelForce), 1)
         p, pp = create_sphere(PosX[i], PosY[i], PosZ[i], RelForce, 1, "phi")
-        #data_theta1.AddInputData(t.GetOutput())
-        #data_theta2.AddInputData(tt.GetOutput())
+
         data_phi1.AddInputData(p.GetOutput())
         data_phi2.AddInputData(pp.GetOutput())
 
-    #data_theta1.Update()
-    #data_theta2.Update()
     data_phi1.Update()
     data_phi2.Update()
 
-    #return  (data_theta1, data_theta2, data_phi1, data_phi2)
     return  (data_phi1, data_phi2)
-    #return  (data_theta1, data_theta2)
 
 def write_vtp(file, data):
     writer = vtk.vtkXMLPolyDataWriter()
@@ -105,36 +97,21 @@ def work(infile):
     # read csv input file and extract data to arrays
     PosX, PosY, PosZ, HForces, CForces, LForces = read_csv(infile)
 
-    #if infile == '../data100.csv':
-        #print(PosX, len(PosX))
-
     # extract timestep
     timestep = re.findall(r'\d+', infile)
 
-    #t, tt, p, pp = generate_spheres(HForces, CForces, LForces, \
-    #t, tt = generate_spheres(HForces, CForces, LForces, \
     p, pp = generate_spheres(HForces, CForces, LForces, \
         PosX, PosY, PosZ, HForces)
-    #write_vtp("spheres_theta1_" + "HForces" + str(timestep[0]) + ".vtp", t)
-    #write_vtp("spheres_theta2_" + "HForces" + str(timestep[0]) + ".vtp", tt)
     write_vtp("spheres_phi1_" + "HForces" + str(timestep[0]) + ".vtp", p)
     write_vtp("spheres_phi2_" + "HForces" + str(timestep[0]) + ".vtp", pp)
 
-    #t, tt, p, pp = generate_spheres(HForces, CForces, LForces, \
-    #t, tt = generate_spheres(HForces, CForces, LForces, \
     p, pp = generate_spheres(HForces, CForces, LForces, \
         PosX, PosY, PosZ, CForces)
-    #write_vtp("spheres_theta1_" + "CForces" + str(timestep[0]) + ".vtp", t)
-    #write_vtp("spheres_theta2_" + "CForces" + str(timestep[0]) + ".vtp", tt)
     write_vtp("spheres_phi1_" + "CForces" + str(timestep[0]) + ".vtp", p)
     write_vtp("spheres_phi2_" + "CForces" + str(timestep[0]) + ".vtp", pp)
 
-    #t, tt, p, pp = generate_spheres(HForces, CForces, LForces, \
-    #t, tt = generate_spheres(HForces, CForces, LForces, \
     p, pp = generate_spheres(HForces, CForces, LForces, \
         PosX, PosY, PosZ, LForces)
-    #write_vtp("spheres_theta1_" + "LForces" + str(timestep[0]) + ".vtp", t)
-    #write_vtp("spheres_theta2_" + "LForces" + str(timestep[0]) + ".vtp", tt)
     write_vtp("spheres_phi1_" + "LForces" + str(timestep[0]) + ".vtp", p)
     write_vtp("spheres_phi2_" + "LForces" + str(timestep[0]) + ".vtp", pp)
 
