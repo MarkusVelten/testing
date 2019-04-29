c268fcdbb7581f34cbacd89b5e1d63b329c9baa7
Markus Velten <markus.velten@tu-dresden.de>
2019-04-29 17:46:32 +0200

    create sphere-parts such that all three parts can be properly combined to one sphere

--- nbody/build/visualization/spheres.py
+++ nbody/build/visualization/spheres.py
@@ -8,52 +8,71 @@ import re
 from multiprocessing import cpu_count
 from joblib import Parallel, delayed
 
-def create_sphere(PosX, PosY, PosZ, force_p, size, size_factor=0.00001):
-    s = vtk.vtkSphereSource()
-    s.SetRadius(size * size_factor)
-
-    s.SetCenter(PosX, PosY, PosZ)
-
-    s.SetStartPhi(0)
-    s.SetEndPhi(force_p * 180.0)
-
-    s.Update()
-
-    ss = vtk.vtkSphereSource()
-    ss.SetRadius(size * size_factor)
-
-    ss.SetCenter(PosX, PosY, PosZ)
-
-    ss.SetStartPhi(force_p * 180.0)
-    ss.SetEndPhi(180.0)
-
-    ss.Update()
-
-    return (s, ss)
-
 # centers == positions
 # force_data == forces
-def generate_spheres(HForce, CForce, LForce, PosX, PosY, PosZ, force):
-    data_phi1 = vtk.vtkAppendPolyData()
-    data_phi2 = vtk.vtkAppendPolyData()
+def generate_spheres(HForce, CForce, LForce, PosX, PosY, PosZ):
+    data_phiH = vtk.vtkAppendPolyData()
+    data_phiC = vtk.vtkAppendPolyData()
+    data_phiL = vtk.vtkAppendPolyData()
+
+    size = 1
+    size_factor = 0.00001
 
     for i in range(len(PosX)):
 
         ForceSum = HForce[i] + CForce[i] + LForce[i]
-        if force[i] == 0:
-            RelForce = 0
+        if ForceSum == 0:
+            RelHForce = 0
+            RelCForce = 0
+            RelLForce = 0
         else:
-            RelForce = force[i] / ForceSum
+            RelHForce = HForce[i] / ForceSum
+            RelCForce = CForce[i] / ForceSum
+            RelLForce = LForce[i] / ForceSum
 
-        p, pp = create_sphere(PosX[i], PosY[i], PosZ[i], RelForce, 1)
+        # create sphere-part for harmonic forces
+        sphereH = vtk.vtkSphereSource()
+        sphereH.SetRadius(size * size_factor)
 
-        data_phi1.AddInputData(p.GetOutput())
-        data_phi2.AddInputData(pp.GetOutput())
+        sphereH.SetCenter(PosX[i], PosY[i], PosZ[i])
 
-    data_phi1.Update()
-    data_phi2.Update()
+        sphereH.SetStartPhi(0)
+        sphereH.SetEndPhi(RelHForce * 180.0)
 
-    return  (data_phi1, data_phi2)
+        sphereH.Update()
+
+        # create sphere-part for coulomb forces
+        sphereC = vtk.vtkSphereSource()
+        sphereC.SetRadius(size * size_factor)
+
+        sphereC.SetCenter(PosX[i], PosY[i], PosZ[i])
+
+        sphereC.SetStartPhi(RelHForce * 180.0)
+        sphereC.SetEndPhi((RelHForce + RelCForce) * 180.0)
+
+        sphereC.Update()
+
+        # create sphere-part for cooling-laser forces
+        sphereL = vtk.vtkSphereSource()
+        sphereL.SetRadius(size * size_factor)
+
+        sphereL.SetCenter(PosX[i], PosY[i], PosZ[i])
+
+        sphereL.SetStartPhi((RelHForce + RelCForce) * 180.0)
+        sphereL.SetEndPhi((RelHForce + RelCForce + RelLForce) * 180.0)
+
+        sphereL.Update()
+
+        data_phiH.AddInputData(sphereH.GetOutput())
+        data_phiC.AddInputData(sphereC.GetOutput())
+        data_phiL.AddInputData(sphereL.GetOutput())
+
+    data_phiH.Update()
+    data_phiC.Update()
+    data_phiL.Update()
+
+    return  (data_phiH, data_phiC, data_phiL)
+
 
 def write_vtp(file, data):
     writer = vtk.vtkXMLPolyDataWriter()
@@ -96,20 +115,11 @@ def work(infile):
     # extract timestep
     timestep = re.findall(r'\d+', infile)
 
-    p, pp = generate_spheres(HForces, CForces, LForces, \
-        PosX, PosY, PosZ, HForces)
-    write_vtp("spheres_phi1_" + "HForces" + str(timestep[0]) + ".vtp", p)
-    write_vtp("spheres_phi2_" + "HForces" + str(timestep[0]) + ".vtp", pp)
-
-    p, pp = generate_spheres(HForces, CForces, LForces, \
-        PosX, PosY, PosZ, CForces)
-    write_vtp("spheres_phi1_" + "CForces" + str(timestep[0]) + ".vtp", p)
-    write_vtp("spheres_phi2_" + "CForces" + str(timestep[0]) + ".vtp", pp)
-
-    p, pp = generate_spheres(HForces, CForces, LForces, \
-        PosX, PosY, PosZ, LForces)
-    write_vtp("spheres_phi1_" + "LForces" + str(timestep[0]) + ".vtp", p)
-    write_vtp("spheres_phi2_" + "LForces" + str(timestep[0]) + ".vtp", pp)
+    p, pp, ppp = generate_spheres(HForces, CForces, LForces, \
+        PosX, PosY, PosZ)
+    write_vtp("spheres_phi_" + "HForces" + str(timestep[0]) + ".vtp", p)
+    write_vtp("spheres_phi_" + "CForces" + str(timestep[0]) + ".vtp", pp)
+    write_vtp("spheres_phi_" + "LForces" + str(timestep[0]) + ".vtp", ppp)
 
 
 def main():
@@ -120,8 +130,6 @@ def main():
 
     Parallel(n_jobs = num_cores)(delayed(work)(infile) \
         for infile in glob.glob(os.path.join(path, 'data*.csv')))
-
-
 
 
 if __name__ == "__main__":
